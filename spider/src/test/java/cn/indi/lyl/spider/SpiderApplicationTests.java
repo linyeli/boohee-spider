01874b140c8a4cf80d2077738a69614742f53da5
linyeli <yelilin89757@gmail.com>
2021-08-23 11:20:22 +0800

    project init

---
+++ spider/src/test/java/cn/indi/lyl/spider/SpiderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.indi.lyl.spider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
